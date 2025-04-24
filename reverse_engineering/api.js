2f9e5e4f5d746408ee07ea1f33aba283afd2f694
Ugo Bechameil <ugo.bechameil@hackolade.com>
2025-04-24 09:20:54 +0200

    Fix/patch (#20)
    * use own lodash
    
    * regenerate gremlin patch
    
    * cleanup

--- reverse_engineering/api.js
+++ reverse_engineering/api.js
@@ -9,12 +9,12 @@ module.exports = {
 	connect: function (_, connectionInfo, sshService, logger, cb) {
 		logger.clear();
 		logger.log('info', connectionInfo, 'connectionInfo', connectionInfo.hiddenKeys);
-		gremlinHelper(_).connect(connectionInfo, sshService).then(cb, cb);
+		gremlinHelper.connect(connectionInfo, sshService).then(cb, cb);
 	},
 
 	disconnect: function (connectionInfo, logger, cb, app) {
 		const sshService = app.require('@hackolade/ssh-service');
-		gremlinHelper(_).close(sshService);
+		gremlinHelper.close(sshService);
 		cb();
 	},
 
@@ -27,7 +27,7 @@ module.exports = {
 				return;
 			}
 
-			gremlinHelper(_)
+			gremlinHelper
 				.testConnection()
 				.then(() => {
 					this.disconnect(connectionInfo, logger, () => {}, app);
@@ -55,18 +55,18 @@ module.exports = {
 			dbName: '',
 			dbCollections: '',
 		};
-		const helper = gremlinHelper(_);
-		helper
+		gremlinHelper
 			.connect(connectionInfo, sshService)
 			.then(
-				() => helper.getLabels(),
+				() => gremlinHelper.getLabels(),
 				error => cb({ message: 'Connection error', stack: error.stack }),
 			)
 			.then(labels => {
+				console.warn('>>>>>>>>>>>>>>>< LABELS', labels);
 				result.dbCollections = labels;
 			})
 			.then(() => {
-				return helper.getDatabaseName();
+				return gremlinHelper.getDatabaseName();
 			})
 			.then(dbName => {
 				result.dbName = dbName;
@@ -98,16 +98,16 @@ module.exports = {
 				let labels = collections[dbName];
 				let metaData = {};
 
-				gremlinHelper(_)
+				gremlinHelper
 					.getFeatures()
 					.then(features => {
 						metaData.features = features;
 					})
-					.then(() => gremlinHelper(_).getVariables())
+					.then(() => gremlinHelper.getVariables())
 					.then(variables => {
 						metaData.variables = variables;
 					})
-					.then(() => gremlinHelper(_).getIndexes())
+					.then(() => gremlinHelper.getIndexes())
 					.then(indexes => {
 						logger.progress({
 							message: `Indexes successfully retrieved`,
@@ -133,7 +133,7 @@ module.exports = {
 							(result, packageData) => result.concat([packageData.collectionName]),
 							[],
 						);
-						return gremlinHelper(_).getRelationshipSchema(labels);
+						return gremlinHelper.getRelationshipSchema(labels);
 					})
 					.then(schema => {
 						return schema.filter(data => {
@@ -194,7 +194,7 @@ const getNodesData = (dbName, labels, logger, data) => {
 			labels,
 			(labelName, nextLabel) => {
 				logger.progress({ message: 'Start sampling data', containerName: dbName, entityName: labelName });
-				gremlinHelper(_)
+				gremlinHelper
 					.getNodesCount(labelName)
 					.then(quantity => {
 						logger.progress({
@@ -204,11 +204,11 @@ const getNodesData = (dbName, labels, logger, data) => {
 						});
 						const count = getSampleDocSize(quantity, data.recordSamplingSettings);
 
-						return gremlinHelper(_)
+						return gremlinHelper
 							.getNodes(labelName, count)
 							.then(documents => ({ limit: count, documents }));
 					})
-					.then(({ documents, limit }) => gremlinHelper(_).getSchema('V', documents, labelName, limit))
+					.then(({ documents, limit }) => gremlinHelper.getSchema('V', documents, labelName, limit))
 					.then(({ documents, schema, template }) => {
 						logger.progress({
 							message: `Data successfully retrieved`,
@@ -266,11 +266,11 @@ const getRelationshipData = (schema, dbName, recordSamplingSettings, fieldInfere
 		async.map(
 			schema,
 			(chain, nextChain) => {
-				gremlinHelper(_)
+				gremlinHelper
 					.getCountRelationshipsData(chain.start, chain.relationship, chain.end)
 					.then(quantity => {
 						const count = getSampleDocSize(quantity, recordSamplingSettings);
-						return gremlinHelper(_).getRelationshipData(chain.start, chain.relationship, chain.end, count);
+						return gremlinHelper.getRelationshipData(chain.start, chain.relationship, chain.end, count);
 					})
 					.then(({ documents, schema, template }) => {
 						let packageData = {
